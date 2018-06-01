2497af35e7090197c27c37c0cd0a2b1a561af132
Jochem Smit <j.h.smit@rug.nl>
2018-06-01 10:59:08 +0200

    refactor setup to atomicplot

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='atomicplot2',
+    name='atomicplot',
     version='0.0.1',
     description='Plotting in Jupyter notebooks with the power of Atom',
     author='Jochem Smit',
     author_email='jhsmit@gmail.com',
-    packages=['atomicplot2'],
+    packages=['atomicplot'],
     keywords='plotting jupyter atom',
     #install requires = [ lots of stuff ]
 )
