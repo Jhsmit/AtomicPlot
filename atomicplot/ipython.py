551a79b899008408e1126e67ee690a87b0aa6e15
Jochem Smit <j.h.smit@rug.nl>
2018-06-29 16:29:59 +0200

    added vmin vmax slider to ImgSlider

--- atomicplot/ipython.py
+++ atomicplot/ipython.py
@@ -1,7 +1,71 @@
+import matplotlib.pyplot as plt
 from ipywidgets import widgets
 from IPython.display import display
 import numpy as np
+from ipywidgets import interact
+class ImgSlider(object):
+    """view image stacks in jupyter notebook"""
+    #todo pyqtgraph?
 
+    #defaults
+    cmap = 'viridis'
+
+    def __init__(self, img_stack):
+        self.img_stack = img_stack
+        self.fig, self.axes = plt.subplots()
+        self.img_mpl = self.axes.imshow(self.img_stack[0])
+
+        self.idx = 0
+
+        vbox = widgets.VBox()
+
+        frame_slider = widgets.IntSlider(
+            value=0,
+            min=0,
+            max=len(img_stack),
+            step=1,
+            disabled=False,
+            continuous_update=True,
+            orientation='horizontal',
+            readout=True,
+            readout_format='d'
+)
+        frame_slider.observe(self.update, names='value')
+
+        vmin = self.img_stack[0].min()
+        vmax = self.img_stack[0].max()
+        self.vrange_slider = widgets.FloatRangeSlider(
+            value=[vmin, vmax],
+            min=vmin,
+            max=vmax,
+            step=(vmax - vmin) / 1000,
+            disabled=False,
+            continuous_update=True,
+            orientation='horizontal',
+            readout=True,
+            readout_format='.1f',
+        )
+
+        self.vrange_slider.observe(self.v_update, names='value')
+
+        vbox.children = [frame_slider, self.vrange_slider]
+        display(vbox)
+
+    def update(self, change):
+        new = change['new']
+        self.idx = new
+        new_img = self.img_stack[new]
+        self.img_mpl.set_data(new_img)
+
+        vmin, vmax = new_img.min(), new_img.max()
+        self.img_mpl.set_clim(vmin=vmin, vmax=vmax)
+        self.vrange_slider.min = vmin
+        self.vrange_slider.max = vmax
+        self.vrange_slider.value = (vmin, vmax)
+
+    def v_update(self, change):
+        vmin, vmax = change.new
+        self.img_mpl.set_clim(vmin=vmin, vmax=vmax)
 
 class BrowseGraph(object):
     """Does magic on your data in jupyter notebooks"""
